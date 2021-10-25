3391c7932b4c5ac17abfac37f56df9efebe105c9
ianirolab <ianiro.1845351@studenti.uniroma1.it>
2021-10-25 15:06:00 +0200

    project reset

--- disastrOS_queues.c
+++
@@ -1,9 +0,0 @@
-#include "disastrOS_constants.h"
-#include "disastrOS_queues.h"
-#include "disastrOS_globals.h"
-
-dmqd_t dmq_open(const char *name, int dflag, mode_t mode, struct dmq_attr *attr){
-    // create a new resource, with the next id available
-    Resource_alloc(++last_resource_id % MAX_NUM_RESOURCES,QUEUE_RES);
-    //TODO: what if there are no more resources available?
-}
